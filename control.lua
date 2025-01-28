e87b23f805f4501b3dcbb3f3f60bc1e6593e3d9f
ryan <ryan@tocks.org>
2025-01-28 10:28:49 -0700

    updated mod for 2.0 and minor bugfixes

--- control.lua
+++ control.lua
@@ -1,5 +1,7 @@
 local refillTime = 0
 local oilYield = 0
+local sulfuricYield = 0
+local fluorineYield = 0
 local moddedYield = 0
 
 function refillOre(event)
@@ -14,6 +16,14 @@ function refillOre(event)
         resetOre("stone",event)
     elseif event.entity.name == "uranium-ore" then
         resetOre("uranium",event)
+    elseif event.entity.name == "calcite" then
+        resetOre('calcite',event)
+    elseif event.entity.name == "tungsten-ore" then
+        resetOre('tungsten',event)
+    elseif event.entity.name == "scrap" then
+        resetOre('scrap',event)
+    elseif event.entity.name == "lithium-brine" then
+        resetOre('lithium',event)
     else
         resetOre("modded-ores",event)
     end
@@ -29,9 +39,14 @@ function resetYield()
     for _, surface in pairs(game.surfaces) do
         --Finds all resource entities on the current surface
         for _, entity in pairs(surface.find_entities_filtered({type="resource"})) do
-            --Checks if the current entity is oil and if the refill oil setting is on
+            --game.print(entity.name);
+            --Checks if the current entity is oil or a space age resource and fills it if the appropriate setting is on
             if entity.name=="crude-oil" and settings.global["refill-oil"].value == true then
                 entity.amount = oilYield
+            elseif entity.name=="sulfuric-acid-geyser" and settings.global["refill-sulfuric-acid"].value == true then
+                entity.amount = sulfuricYield
+            elseif entity.name=="fluorine-vent" and settings.global["refill-fluorine"].value == true then
+                entity.amount = fluorineYield
             --Checks if the current entity is an infinite resource and if the refill modded yield setting is on
             elseif entity.initial_amount ~= nil and settings.global["refill-modded-yield"].value == true then
                 entity.amount = moddedYield
@@ -43,12 +58,15 @@ function tick(event)
     --The time inputed by the user needs to be converted to game ticks.
     refillTime = settings.global["time-refill"].value * 60
 
-    --The yields that the user inputs is different from the ones displayed in the game, this fixes that.
-    oilYield = settings.global["oil-yield"].value * 3000
-    moddedYield = settings.global["modded-yield"].value * 3000
-
     --Checks if it is time to refill oil
     if event.tick % refillTime == 0 then
+        --The yields that the user inputs is different from the ones displayed in the game, this fixes that.
+        oilYield = settings.global["oil-yield"].value * 3000
+        if script.active_mods['space-age'] then
+            sulfuricYield = settings.global["sulfuric-acid-yield"].value * 3000
+            fluorineYield = settings.global["fluorine-yield"].value * 1000
+        end
+        moddedYield = settings.global["modded-yield"].value * 3000
         resetYield()
     end
 end
--- settings.lua
+++ settings.lua
@@ -1,85 +1,150 @@
 data:extend({
+  {
+    name = "time-refill",
+    type = "int-setting",
+    setting_type = "runtime-global",
+    default_value = 300,
+    minimum_value = 1,
+    maximum_value = 1000000,
+    order = "ba"
+  },
+  {
+    name = "refill-iron",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "ca"
+  },
+  {
+    name = "refill-copper",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "cb"
+  },
+  {
+    name = "refill-coal",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "cd"
+  },
+  {
+    name = "refill-stone",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "ce"
+  },
+  {
+    name = "refill-uranium",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "cf"
+  },
+  {
+    name = "refill-modded-ores",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "cz"
+  },
+  {
+    name = "refill-oil",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "da"
+  },
+  {
+    name = "oil-yield",
+    type = "int-setting",
+    setting_type = "runtime-global",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 1000000,
+    order = "db"
+  },
+  {
+    name = "refill-modded-yield",
+    type = "bool-setting",
+    setting_type = "runtime-global",
+    default_value = true,
+    order = "dy"
+  },
+  {
+    name = "modded-yield",
+    type = "int-setting",
+    setting_type = "runtime-global",
+    default_value = 100,
+    minimum_value = 1,
+    maximum_value = 1000000,
+    order = "dz"
+  }
+});
+
+if mods['space-age'] then
+  data:extend({
     {
-      name = "oil-yield",
+      name = "refill-calcite",
+      type = "bool-setting",
+      setting_type = "runtime-global",
+      default_value = true,
+      order = "cg"
+    },
+    {
+      name = "refill-tungsten",
+      type = "bool-setting",
+      setting_type = "runtime-global",
+      default_value = true,
+      order = "ch"
+    },
+    {
+      name = "refill-scrap",
+      type = "bool-setting",
+      setting_type = "runtime-global",
+      default_value = true,
+      order = "ci"
+    },
+    {
+      name = "refill-lithium",
+      type = "bool-setting",
+      setting_type = "runtime-global",
+      default_value = true,
+      order = "cj"
+    },
+    {
+      name = "refill-sulfuric-acid",
+      type = "bool-setting",
+      setting_type = "runtime-global",
+      default_value = true,
+      order = "dc"
+    },
+    {
+      name = "sulfuric-acid-yield",
       type = "int-setting",
       setting_type = "runtime-global",
       default_value = 100,
       minimum_value = 1,
       maximum_value = 1000000,
-      order = "a"
+      order = "dd"
     },
     {
-      name = "modded-yield",
+      name = "refill-fluorine",
+      type = "bool-setting",
+      setting_type = "runtime-global",
+      default_value = true,
+      order = "de"
+    },
+    {
+      name = "fluorine-yield",
       type = "int-setting",
       setting_type = "runtime-global",
       default_value = 100,
       minimum_value = 1,
       maximum_value = 1000000,
-      order = "b"
-    },
-    {
-      name = "time-refill",
-      type = "int-setting",
-      setting_type = "runtime-global",
-      default_value = 300,
-      minimum_value = 1,
-      maximum_value = 1000000,
-      order = "c"
-    },
-    {
-      name = "refill-iron",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "d"
-    },
-    {
-      name = "refill-copper",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "e"
-    },
-    {
-      name = "refill-coal",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "f"
-    },
-    {
-      name = "refill-stone",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "g"
-    },
-    {
-      name = "refill-uranium",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "h"
-    },
-    {
-      name = "refill-modded-ores",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "j"
-    },
-    {
-      name = "refill-oil",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "i"
-    },
-    {
-      name = "refill-modded-yield",
-      type = "bool-setting",
-      setting_type = "runtime-global",
-      default_value = true,
-      order = "k"
+      order = "df"
     }
   });
+end
